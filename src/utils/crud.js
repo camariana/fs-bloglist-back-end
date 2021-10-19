e7992b52b57f6862c4267efa9e665af77b1248cc
camariana <camariana40@outlook.com>
2021-10-19 17:38:19 +0000

    complete the crud operations

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -1,30 +1,64 @@
-export const getAll = (model) => async (request, response) => {
-  try {
-    const docs = await model
-      .find({})
-    response.json(docs.map(d => d.toJSON()))
-  } catch (error) {
-    console.error(error)
-  }
-}
-
 export const createOne = (model) => async (request, response) => {
   const body = request.body
-  //const createdBy = request.user._id
+  //const createdBy = request.user.id
 
-  try {
-    const doc = await model
-      .create({ ...body })
-    response.json(doc)
-  } catch (error) {
-    console.error(error)
+  const doc = await model
+    .create({ ...body })
+  response.json(doc)
+}
+
+export const readAll = (model) => async (request, response) => {
+  const docs = await model
+    .find({})
+  response.json(docs)
+}
+
+export const readOne = (model) => async (request, response) => {
+  //const createdBy = request.user.id
+  const id = request.params.id
+  const doc = await model
+    .findById(id)
+
+  if(!doc) {
+    return response.status(404).end()
   }
+
+  response.json(doc)
+}
+
+export const updateOne = (model) => async (request, response) => {
+  //const createdBy = request.user.id
+  const id = request.params.id
+  const body = request.body
+
+  const updatedDoc = await model
+    .findByIdAndUpdate(id, body, { new: true })
+
+  if (!updatedDoc) {
+    return response.status(400).end()
+  }
+
+  response.json(updatedDoc)
+}
+
+export const deleteOne = (model) => async (request, response) => {
+  //const createdBy = request.user.id
+  const id = request.params.id
+
+  const removed = await model
+    .findByIdAndRemove(id)
+
+  if(!removed) {
+    return response.status(400).end()
+  }
+
+  response.status(204).end()
 }
 
 export const crudControllers = model => ({
-  //getOne: getOne(model),
-  getAll: getAll(model),
-  createOne: createOne(model)
-  // updateOne: updateOne(model),
-  // removeOne: removeOne(model),
+  createOne: createOne(model),
+  readAll: readAll(model),
+  readOne: readOne(model),
+  updateOne: updateOne(model),
+  deleteOne: deleteOne(model),
 })
