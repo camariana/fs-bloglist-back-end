70ab68c931c2594e7d517d5845fdab7822ab0ff1
camariana <camariana40@outlook.com>
2021-10-19 17:36:05 +0000

    setup development and test db properly

--- src/config/index.js
+++ src/config/index.js
@@ -7,15 +7,22 @@ export const port = process.env.PORT
 export const jwtSecret = process.env.JWT_SECRET
 export const jwtExpires = process.env.JWT_EXPIRE
 
-export let dbUrl = ''
+export const dbUrl = env === 'test'
+  ? process.env.TEST_MONGODB_URI
+  : process.env.MONGODB_URI
 
-switch(env) {
-case 'test':
-  dbUrl = process.env.TEST_MONGODB_URI
-  break
-case 'production':
-  dbUrl = process.env.LIVE_MONGODB_URI
-  break
-default:
-  dbUrl = process.env.LOCAL_MONGODB_URI
-}
+// let dbUrl
+// switch (env) {
+// case 'development':
+//   dbUrl = process.env.MONGODB_URI
+//   break
+
+// case 'test':
+//   dbUrl = process.env.TEST_MONGODB_URI
+//   break
+
+// default:
+//   dbUrl = process.env.LIVE_MONGODB_URI
+// }
+
+// export default  dbUrl
