82a23ef7a850ee14431930d47b308ff5fb48c4b1
camariana <camariana40@outlook.com>
2021-10-19 17:37:57 +0000

    turn the _id object to id

--- src/resources/blog/blog.model.js
+++ src/resources/blog/blog.model.js
@@ -19,4 +19,12 @@ const blogSchema = new mongoose.Schema(
   { timestamps: true }
 )
 
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
 export const Blog = mongoose.model('blog', blogSchema)
