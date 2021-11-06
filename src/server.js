50a0e0135299032a7cad5b1e21d960c0c4e84a20
camariana <camariana40@outlook.com>
2021-11-06 00:49:47 +0000

    add user extractor and login route

--- src/server.js
+++ src/server.js
@@ -7,7 +7,9 @@ import 'express-async-errors'
 
 import { unknownEndpoint, errorHandler, requestLogger } from './utils/middleware'
 import { info, error as _error } from './utils/logger'
+import { tokenExtractor } from './utils/auth'
 
+import { login } from './resources/login/login'
 import blogRouter from './resources/blog/blog.router'
 import userRouter from './resources/user/user.router'
 import commentRouter from './resources/comment/comment.router'
@@ -16,14 +18,18 @@ const app = express()
 connect() //TODO remove this after doing the tests
 
 app.use(cors())
-app.use(json())
-app.use(morgan('dev'))
+app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 
 app.use(requestLogger)
+app.use(tokenExtractor)
+
 
 app.use('/api/blog', blogRouter)
 app.use('/api/comment', commentRouter)
-app.use('/user', userRouter)
+app.use('/api/user', userRouter)
+
+app.post('/login', login)
 
 app.use(unknownEndpoint)
 app.use(errorHandler)
